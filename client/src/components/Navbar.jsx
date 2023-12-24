04c0b8194a0eaf9aa170a133bc32fbd3c854e095
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-25 01:23:46 +0600

    Logo name changed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
             <circle cx="16.9857" cy="17.4857" r="12.0143" fill="#3575E2" />
           </svg>
           {""}
-          <span>JobPortal</span>{" "}
+          <span>JobSynergy</span>{" "}
         </a>
         {/* nav items for large devices */}
         <ul className="hidden md:flex gap-12 ">
