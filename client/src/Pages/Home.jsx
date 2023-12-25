488bd9e2475e8707ba8719a52ba3850a78693ad7
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-25 16:08:34 +0600

    filter , card, right - divided by these section

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -77,12 +77,12 @@ const Home = () => {
       <Banner query={query} handleInputChange={handleInputChange} />
       {/* main contents */}
       <div className="bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-1 ">
-        <div>Left</div>
-        <div>
+        <div className="bg-white p-4 rounded">Left</div>
+        <div className="col-span-2 bg-white p-4 rounded">
           {" "}
           <Jobs result={result} />
         </div>
-        <div>Right</div>
+        <div className="bg-white p-4 rounded">Right</div>
       </div>
     </div>
   );
