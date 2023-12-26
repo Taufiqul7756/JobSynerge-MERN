8519e40723c5fd323a3746e142c14dea4a0e66a0
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-26 23:14:28 +0600

    About compo

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Banner from "../components/Banner";
 
 const About = () => {
-  return;
+  return (
+    <>
+      <h2>About</h2>
+    </>
+  );
 };
 
 export default About;
