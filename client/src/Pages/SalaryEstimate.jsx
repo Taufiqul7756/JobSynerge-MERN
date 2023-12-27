cbf4358eb94c99f9df7d946ae4a8c22f23d4e038
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-28 00:13:13 +0600

    SalaryEstimate component - updated

--- client/src/Pages/SalaryEstimate.jsx
+++ client/src/Pages/SalaryEstimate.jsx
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PageHeader from "../components/PageHeader";
 
 const SalaryEstimate = () => {
-  const [serachText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [salary, setSalary] = useState([]);
+
+  useEffect(() => {
+    fetch("salary.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setSalary(data);
+      });
+  }, []);
+
+  console.log(searchText);
+
+  const handleSearch = () => {
+    const filter = salary.filter(
+      (job) =>
+        job.jobTitle.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+    );
+    console.log(filter);
+    setSalary(filter);
+  };
 
   return (
     <div className="max-w-screen-2xl container mx-auto xl:pax-24 px-4">
@@ -18,7 +38,10 @@ const SalaryEstimate = () => {
             placeholder=""
             className="py-2 pl-3 border focus:outline-none lg:w-6/12 mb-4 w-full"
           />
-          <button className="bg-blue text-white font-semibold px-8 py-2 rounded-sm mb-4">
+          <button
+            onClick={handleSearch}
+            className="bg-blue text-white font-semibold px-8 py-2 rounded-sm mb-4"
+          >
             Search
           </button>
         </div>
